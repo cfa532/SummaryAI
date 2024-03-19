32b59dd50f31ad3bf5908be0beaab2d85675b9d8
cfa532 <cfa532@yahoo.com>
2024-03-19 23:25:35 +0800

    Initial Commit

---
+++ SummaryAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SummaryAI
+//
+//  Created by 超方 on 2024/3/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SummaryAI/SummaryAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummaryAIApp.swift
+//  SummaryAI
+//
+//  Created by 超方 on 2024/3/19.
+//
+
+import SwiftUI
+
+@main
+struct SummaryAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
